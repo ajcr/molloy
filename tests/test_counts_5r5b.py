daf9989600017cd81719ff29c83f38fa5dd96aad
ajcr <ajcriley@gmail.com>
2017-07-01 21:08:16 +0100

    Add tests using other operators

--- tests/test_counts_5r5b.py
+++ tests/test_counts_5r5b.py
@@ -18,6 +18,17 @@ def test_5r5b_constrain_red_eq(size, red, expected):
     constraints = 'red == {}'.format(red)
     assert collection_5r5b.count_sets(size, constraints) == expected
 
+# 5 red, 5 blue, constrain red (!=)
+@pytest.mark.parametrize('size,red,expected', [
+    (10, 5, 0),
+    ( 9, 5, 1),
+    ( 1, 1, 1),
+    ( 6, 3, 4),
+])
+def test_5r5b_constrain_red_neq(size, red, expected):
+    constraints = 'red != {}'.format(red)
+    assert collection_5r5b.count_sets(size, constraints) == expected
+
 # 5 red, 5 blue, constrain red (<)
 @pytest.mark.parametrize('size,red,expected', [
     (10, 6, 1),
@@ -43,6 +54,56 @@ def test_5r5b_constrain_red_lt(size, red, expected):
 ])
 def test_5r5b_constrain_red_ge(size, red, expected):
     constraints = 'red >= {}'.format(red)
+    assert collection_5r5b.count_sets(size, constraints) == expected
+
+# 5 red, 5 blue, constrain red (in)
+@pytest.mark.parametrize('size,red,expected', [
+    (10, [5], 1),
+    ( 9, [5], 1),
+    ( 8, [5], 1),
+    (10, [4], 0),
+    ( 6, [1], 1),
+    ( 5, [0], 1),
+    ( 0, [1], 0),
+    (10, [5, 1], 1),
+    ( 9, [5, 2], 1),
+])
+def test_5r5b_constrain_red_in(size, red, expected):
+    constraints = 'red in {}'.format(red)
+    assert collection_5r5b.count_sets(size, constraints) == expected
+
+# 5 red, 5 blue, constrain red (not in)
+@pytest.mark.parametrize('size,red,expected', [
+    (10, [5], 0),
+    ( 9, [5], 1),
+    ( 1, [1], 1),
+    ( 6, [3], 4),
+    ( 5, [1, 2], 4),
+])
+def test_5r5b_constrain_red_not_in(size, red, expected):
+    constraints = 'red not in {}'.format(red)
+    assert collection_5r5b.count_sets(size, constraints) == expected
+
+# 5 red, 5 blue, constrain red (%, ==)
+@pytest.mark.parametrize('size,mod,rem,expected', [
+    (10, 2, 0, 0),
+    (10, 2, 1, 1),
+    ( 5, 2, 0, 3),
+    ( 5, 2, 1, 3),
+    ( 5, 3, 0, 2),
+])
+def test_5r5b_constrain_red_modulo_eq(size, mod, rem, expected):
+    constraints = 'red % {} == {}'.format(mod, rem)
+    assert collection_5r5b.count_sets(size, constraints) == expected
+
+# 5 red, 5 blue, constrain red (%, <)
+@pytest.mark.parametrize('size,mod,rem,expected', [
+    ( 5, 2, 1, 3),
+    ( 5, 2, 2, 6),
+    ( 5, 3, 2, 4),
+])
+def test_5r5b_constrain_red_modulo_lt(size, mod, rem, expected):
+    constraints = 'red % {} < {}'.format(mod, rem)
     assert collection_5r5b.count_sets(size, constraints) == expected
 
 # ------------------------------------------------
